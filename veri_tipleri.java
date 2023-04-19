88f0bb7192942a319ca52e8753b66403697b9c94
ahmetkc58 <116435949+ahmetkc58@users.noreply.github.com>
2023-04-20 02:46:49 +0300

    java

---
+++ veri_tipleri.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medium;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class veri_tipleri {
+    
+    public static void main (String [] args ){
+        
+        byte Byte ='1';
+        byte Byte1=-121;
+        // bu ikisi arasındaki fark 1. yazdığım 1 yazı olarak memory de depolanır fakat 2. 1 sayı olarak memory de depolanır 
+        
+        short sayı=32;
+        
+        int sayı1=34;
+        Integer sayı2=901232;
+
+        long  sayı3 =234324324;
+        
+        float sayı4 =-8978789;
+        
+        double sayı5=-894556237;
+        
+        char a ='a';
+        
+        String txt="merhabaler nasılsınız";
+        
+    }
+}
